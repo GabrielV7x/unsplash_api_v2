145dce66a1620d68aff43683db9219780abcf463
Frederic Feliciane <fredericfeliciane8@gmail.com>
2023-10-03 23:04:20 +0400

    personal fetch api code written

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 const apiKey = import.meta.env.VITE_API_KEY
 
 function App() {
  // URL to the Unsplash API endpoint
-//  const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}`;
+ const apiUrl = `https://api.unsplash.com/photos?client_id=${apiKey}`;
 
-//  // Function to fetch and display an image
-// async function fetchAndDisplayImage() {
+ // Function to fetch and display an image
+async function fetchAndDisplayImage() {
 //  try {
 //      const response = await fetch(apiUrl);
 //      if (!response.ok) {
@@ -29,33 +29,52 @@ function App() {
 //      console.error(error);
 //  }
 // }
+  const per_page=10;
 
-//  // Fetch and display an image when the page loads
-// fetchAndDisplayImage();
+  const response = await fetch(`${apiUrl}&per_page=${per_page}`);
+  // console.log(response.json());
+  const data = await response.json();
+  console.log(data.length);
 
-setTimeout(()=> {
-  const imageContainer = document.getElementById('image-container');
-  imageContainer.innerHTML = '<h1>Test</h1>';
-},
-2000)
+  for (let img in data) {
+    console.log(data[img].urls.regular);
+    const imageUrl = data[img].urls.regular;
+    const image = document.createElement('img');
+    image.src = imageUrl;
+    const imageContainer = document.getElementById('image-container');
 
-setTimeout(()=> {
-  const imageContainer = document.getElementById('image-container2');
-  imageContainer.innerHTML = '<h1>blublu</h1>';
-},
-4000)
+    imageContainer.appendChild(image);
 
-const [color, setColor] = useState("red")
+  }
+
+
+  // if (!response.ok) {
+  //     throw new Error('Failed to fetch image');
+  // }
+  // const data = await response.json();
+
+  // // Get the image URL from the response
+  // const imageUrl = data.urls.regular;
+
+  // // Create an image element and set its source
+  // const image = document.createElement('img');
+  // image.src = imageUrl;
+
+  // // Display the image in the container
+  // const imageContainer = document.getElementById('image-container');
+  // imageContainer.innerHTML = '';
+  // imageContainer.appendChild(image);
+
+}
+
+ // Fetch and display an image when the page loads
+fetchAndDisplayImage();
 
 
 return (
-  <>
-    <div id="image-container"></div>
-    <div id="image-container2"></div>
-    <button onClick={()=>setColor("blue")}>Click Me</button>
-    <div>{color}</div>
-
-  </>
+<>
+ <div id="image-container"></div>
+</>
 )
 }
 
