04e08526be5695d847cb95cc60e9ceedc4bd289d
Frederic Feliciane <fredericfeliciane8@gmail.com>
2023-10-03 23:29:23 +0400

    style added

--- src/App.jsx
+++ src/App.jsx
@@ -1,81 +1,37 @@
 // import { useState } from 'react'
+import './styles/main.css';
+
 const apiKey = import.meta.env.VITE_API_KEY
 
 function App() {
  // URL to the Unsplash API endpoint
  const apiUrl = `https://api.unsplash.com/photos?client_id=${apiKey}`;
 
- // Function to fetch and display an image
-async function fetchAndDisplayImage() {
-//  try {
-//      const response = await fetch(apiUrl);
-//      if (!response.ok) {
-//          throw new Error('Failed to fetch image');
-//      }
-//      const data = await response.json();
+  // Function to fetch and display an image
+  async function fetchAndDisplayImage() {
 
-//      // Get the image URL from the response
-//      const imageUrl = data.urls.regular;
+    const per_page=10;
+    const response = await fetch(`${apiUrl}&per_page=${per_page}`);
+    const data = await response.json();
 
-//      // Create an image element and set its source
-//      const image = document.createElement('img');
-//      image.src = imageUrl;
-
-//      // Display the image in the container
-//      const imageContainer = document.getElementById('image-container');
-//      imageContainer.innerHTML = '';
-//      imageContainer.appendChild(image);
-//  } catch (error) {
-//      console.error(error);
-//  }
-// }
-  const per_page=10;
-
-  const response = await fetch(`${apiUrl}&per_page=${per_page}`);
-  // console.log(response.json());
-  const data = await response.json();
-  console.log(data.length);
-
-  for (let img in data) {
-    console.log(data[img].urls.regular);
-    const imageUrl = data[img].urls.regular;
-    const image = document.createElement('img');
-    image.src = imageUrl;
-    const imageContainer = document.getElementById('image-container');
-
-    imageContainer.appendChild(image);
-
+    for (let img in data) {
+      console.log(data[img].urls.regular);
+      const imageUrl = data[img].urls.regular;
+      const image = document.createElement('img');
+      image.src = imageUrl;
+      const imageContainer = document.getElementById('image-container');
+      imageContainer.appendChild(image);
+    }
   }
 
+  // Fetch and display an image when the page loads
+  fetchAndDisplayImage();
 
-  // if (!response.ok) {
-  //     throw new Error('Failed to fetch image');
-  // }
-  // const data = await response.json();
-
-  // // Get the image URL from the response
-  // const imageUrl = data.urls.regular;
-
-  // // Create an image element and set its source
-  // const image = document.createElement('img');
-  // image.src = imageUrl;
-
-  // // Display the image in the container
-  // const imageContainer = document.getElementById('image-container');
-  // imageContainer.innerHTML = '';
-  // imageContainer.appendChild(image);
-
-}
-
- // Fetch and display an image when the page loads
-fetchAndDisplayImage();
-
-
-return (
-<>
- <div id="image-container"></div>
-</>
-)
+  return (
+    <>
+      <div id="image-container"></div>
+    </>
+  )
 }
 
 export default App
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import dotenv from 'dotenv';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sass from 'vite-plugin-sass';
 
 // Load environment variables from .env
 dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    sass(),],
 })
